36fbe3aab23236c17d1390d952be9f1c40f517ee
mila <vorga88@google.com>
2018-11-28 13:40:45 +0200

    add css

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,23 @@
 import "@babel/polyfill";
 import "../scss/main.scss";
+
+window.addEventListener('load', function () {
+    var navs = document.querySelectorAll(".js-nav");
+    var contentItems = document.querySelectorAll(".js-content-item");
+
+    navs.forEach(function (el) {
+        el.addEventListener('change', function () {
+
+            var prev = document.querySelector(".active");
+            var current = document.querySelector("." + el.dataset.id);
+
+            if (prev && current) {
+                if (prev != current) {
+                    prev.classList.toggle("active");
+                    current.classList.toggle("active");
+                }
+            }
+        });
+    });
+
+});
